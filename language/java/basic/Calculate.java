4ca12959e1c46257363b9d57857a9583824d6c40
hanyudeye8 <hanyudeye8@gmail.com>
2021-09-06 08:02:57 +0800

    h

---
+++ language/java/basic/Calculate.java
@@ -0,0 +1,10 @@
+// 计算的一些方法
+
+class Calculate
+{
+    public static void main(String[] args)
+    {
+        double s=   Math.sin(Math.PI/2);
+        System.out.println(s);
+    }
+}
---
+++ language/java/basic/Zero.java
@@ -0,0 +1,9 @@
+// 第一个java 程序
+
+class Zero
+{
+    public static void main(String[] args)
+    {
+        System.out.println("This is first java program!");
+    }
+}
